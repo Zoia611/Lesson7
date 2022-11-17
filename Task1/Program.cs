32eec6463bcd633638c4ec6e529b538fdae3cb51
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-17 14:11:41 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static double Triangle (double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны первого треугольника");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите стороны второго треугольника");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            if (Triangle(x1, y1, z1) > Triangle(x2, y2, z2)) 
+                Console.WriteLine("Площадь второго прямоугольника больше");
+            else if (Triangle(x1, y1, z1) < Triangle(x2, y2, z2))
+                Console.WriteLine("Площадь первого прямоугольника больше");
+            else
+                Console.WriteLine("Площади треугольников равны");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void GetParamsKube(double a, out double s, out double v)
+        {
+            s = 6 * a * a;
+            v = a * a * a;
+        }
+        static void Main(string[] args)         
+        {
+            Console.WriteLine("Введите длинну ребра");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetParamsKube(a, out s, out v);
+            Console.WriteLine("{0} {1}",s,v);
+            Console.ReadLine();
+
+        }
+    }
+}
